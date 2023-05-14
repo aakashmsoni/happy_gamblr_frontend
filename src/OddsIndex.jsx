90077f34feb96ccd36091284e0299d0d8f11a85e
Aakash Soni <inos92@gmail.com>
2023-05-14 03:50:34 -0500

    updated OddsIndex to show only moneylines from each bookmaker for each game

--- src/OddsIndex.jsx
+++ src/OddsIndex.jsx
@@ -4,16 +4,16 @@ export function OddsIndex(props) {
   return (
     <div>
       {props.odds.map(game => (
-        <div key={game.id} className="card w-75 mx-auto">
+        <div key={game.id} className="card w-75 mx-auto mb-5">
           <table className="table table-bordered">
             <thead>
               <tr>
                 <th scope="col" colSpan={3}>
-                  {game.sport_title}
+                  {game.sport_title} - Moneyline
                 </th>
-                <th scope="col">Moneyline</th>
-                <th scope="col">Spread</th>
-                <th scope="col">Total</th>
+                <th scope="col">{game.bookmakers[0].title}</th>
+                <th scope="col">{game.bookmakers[1].title}</th>
+                <th scope="col">{game.bookmakers[2].title}</th>
               </tr>
             </thead>
             <tbody className="table-group-divider">
@@ -22,32 +22,16 @@ export function OddsIndex(props) {
                   {game.home_team}
                 </th>
                 <td>{game.bookmakers[0].markets[0].outcomes[0].price}</td>
-                <td>
-                  {game.bookmakers[0].markets[1].outcomes[0].point}
-                  <br />
-                  {game.bookmakers[0].markets[1].outcomes[0].price}
-                </td>
-                <td>
-                  O {game.bookmakers[0].markets[2].outcomes[1].point}
-                  <br />
-                  {game.bookmakers[0].markets[2].outcomes[0].price}
-                </td>
+                <td>{game.bookmakers[1].markets[0].outcomes[0].price}</td>
+                <td>{game.bookmakers[2].markets[0].outcomes[0].price}</td>
               </tr>
               <tr>
                 <th scope="row" colSpan={3}>
                   {game.away_team}
                 </th>
                 <td>{game.bookmakers[0].markets[0].outcomes[1].price}</td>
-                <td>
-                  {game.bookmakers[0].markets[1].outcomes[1].point}
-                  <br />
-                  {game.bookmakers[0].markets[1].outcomes[1].price}
-                </td>
-                <td>
-                  U {game.bookmakers[0].markets[2].outcomes[1].point}
-                  <br />
-                  {game.bookmakers[0].markets[2].outcomes[1].price}
-                </td>
+                <td>{game.bookmakers[1].markets[0].outcomes[1].price}</td>
+                <td>{game.bookmakers[2].markets[0].outcomes[1].price}</td>
               </tr>
               <tr>
                 <caption className="row w-75 ms-0">{moment(game.commence_time).format("lll")}</caption>
